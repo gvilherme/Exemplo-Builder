1f43e98c806627b027c5d00887e98d42c7413f1f
Guilherme Rodrigues <rodrigues.guilherme@aluno.ufabc.edu.br>
2020-07-13 20:42:27 -0300

    Projeto inicial contendo exemplo de builder

--- BuilderExample/Program.cs
+++ BuilderExample/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BuilderExample.Builders;
+using BuilderExample.Entities;
+using System;
+using System.Collections.Generic;
 
 namespace BuilderExample
 {
@@ -6,7 +9,15 @@ namespace BuilderExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<SaleProduct> products = new List<SaleProduct>(new SaleProduct[] { new SaleProduct("item 1",1,400.0,2)
+                , new SaleProduct("item 2", 2, 500.0, 2), new SaleProduct("item 3",3,600.0,1) });
+            CheckoutBuilder checkoutBuilder = new CheckoutBuilder();
+            checkoutBuilder.AddProducts(products).
+                BuyerCNPJ("357,464,808-19").
+                BuyerName("Braia").
+                HaveTheObs("Testing.");
+            var checkout = checkoutBuilder.Build();
+            Console.WriteLine(checkout);
         }
     }
 }
